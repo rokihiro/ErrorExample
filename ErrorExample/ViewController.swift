319f9ed3b393dd18573de0f09112a8c32960ee03
hkato <rokihiro@gmail.com>
2018-08-20 17:46:27 +0900

    Initial Commit

---
+++ ErrorExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ErrorExample
+//
+//  Created by rokihiro on 2018/08/20.
+//  Copyright © 2018年 rokihiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
